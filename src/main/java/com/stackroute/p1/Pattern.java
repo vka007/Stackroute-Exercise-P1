af7074cff666b02e8646a088a82152d7e82282b0
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-07-03 15:08:02 +0530

    Added comments!

--- src/main/java/com/stackroute/p1/Pattern.java
+++ src/main/java/com/stackroute/p1/Pattern.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 
 public class Pattern {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {                                        //main method
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
-        String result = printPattern(n);
+        String result = printPattern(n);                                            //method call 
 
         System.out.println(result);
 
     }
-
-    public static String printPattern(int n){
-
+                
+    public static String printPattern(int n){                                       //method body
+    
         String s = "";
 
 
 
-        for(int j=1; j<=n ; j++)
+        for(int j=1; j<=n ; j++)                                                       //loop to generate pattern
         {
             for (int k = 1; k<=j; k++)
             {
@@ -29,6 +29,6 @@ public class Pattern {
             }
         }
 
-        return s.trim();
+        return s.trim();                                                            //return the pattern as string
     }
 }
